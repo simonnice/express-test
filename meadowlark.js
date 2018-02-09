4048d0b9b8f6ba991ca07fdb90bac789dd4597d0
Simon <sb22ix@student.lnu.se>
2018-02-09 11:44:23 +0100

    Initial commit

---
+++ meadowlark.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+
+// Set up handlebars view engine
+let handlebars = require('express-handlebars')
+            .create({defaultLayout:'main' })
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+//  Express is also providing us a 
+//  convenience method, res.type, 
+//  which sets the Content-Type header
+
+//res.type('text/plain') // Sets Content type header
+//res.send('Meadowlark Travel') // Express extension
+// These two are not needed when views have been rendered through
+// handlebars because handlebars view engine handles content type
+// and status code 200 by default.
+// Instead we use res.render() to render the correct view
+
+// Setting upp example of dynamic information - 
+//Fortune cookie array
+
+let fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.", 
+]
+
+app.set('port', process.env.PORT || 3000)
+
+// Declaring static middleware for public files JS, CSS, Images
+app.use(express.static(__dirname + '/public'));
+
+
+// Home Page Route
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// About Page Route - 
+// rendering a variable containing random index of fortune
+app.get('/about', (req, res) => {
+    let randomFortune = 
+                fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
+})
+
+// 404 catch-all handler page (middleware)
+app.use((req, res) => { // app.use here instead because it is Middleware
+    res.status(404)
+    res.render('404')
+})
+
+// 500 error handler page (middleware)
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(app.get('port'), function () {
+    console.log(`Express started on http://localhost:${app.get('port')} ; press ctrl-c to terminate.`)
+} )
